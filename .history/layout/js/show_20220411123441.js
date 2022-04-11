0301f05472f3a8c528453c4ea8af9978a15fddca
special4Party <spicial.party@gmail.com>
2022-04-11 17:51:51 +0300

    design display page

---
+++ .history/layout/js/show_20220411123441.js
@@ -0,0 +1,8 @@
+
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
---
+++ .history/layout/js/show_20220411145400.js
@@ -0,0 +1,9 @@
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
