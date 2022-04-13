f761318837ec740e6c44f6519ada12cda06ae5de
special4Party <spicial.party@gmail.com>
2022-04-14 01:02:21 +0300

    2

---
+++ .history/layout/js/dashboard_20220413184349.js
@@ -0,0 +1,14 @@
+const my_body=document.querySelector("body");
+  mode_toggle=my_body.querySelector("body");
+
+mode_toggle.addEventListener("click",()=>{
+
+});
+
+
+
+
+
+
+
+
---
+++ .history/layout/js/dashboard_20220413184523.js
@@ -0,0 +1,14 @@
+const my_body=document.querySelector("body");
+  mode_toggle=my_body.querySelector(".mode-toggle");
+
+mode_toggle.addEventListener("click",()=>{
+    my_body.classList.toggle("Dark");
+});
+
+
+
+
+
+
+
+
