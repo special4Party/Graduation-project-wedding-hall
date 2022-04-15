d4adc8e538c427d37f875ca92c25f251588e1fb6
special4Party <spicial.party@gmail.com>
2022-04-16 01:52:15 +0300

    admin page finish

---
+++ .history/layout/js/dashboard_20220416012341.js
@@ -0,0 +1,17 @@
+const my_body=document.querySelector("body");
+  mode_toggle=my_body.querySelector(".mode-toggle");
+  sidebar=my_body.querySelector("nav");
+  sidebar_toggle=my_body.querySelector(".sidebar-toggle");
+  mode_toggle.addEventListener("click",()=>{
+    my_body.classList.toggle("Dark");
+});
+
+sidebar_toggle.addEventListener("click",()=>{
+  sidebar.classList.toggle("close");
+});
+
+
+
+
+
+
