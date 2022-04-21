e4f66428b1b7c079a97724c608d627d1695d19f1
special4Party <spicial.party@gmail.com>
2022-04-21 16:58:46 +0300

    register owner

---
+++ .history/layout/js/custome_20220418211431.js
@@ -0,0 +1,30 @@
+//scroll qoatation
+function autoScroll(){
+$(".scroll .active-scroll").each(function(){
+
+    if(!$(this).is(':last-child')){
+        $(this).delay(2000).fadeOut(700,function(){
+            $(this).removeClass('active-scroll').next().addClass('active-scroll').fadeIn();
+            // document.getElementsByClassName('active')[1].style.backgroundColor='#777eee';
+            // document.getElementById('p1').nextElementSibling.style.backgroundColor='#777eee';
+            autoScroll();
+        });
+        
+    }else{
+        $(this).delay(1500).fadeOut(700,function(){
+            $(this).removeClass('active-scroll');
+            $('.scroll div').eq(0).addClass('active-scroll').fadeIn();
+            autoScroll();
+        });
+    }
+});
+}();
+
+//----------------------------------
+
+
+
+
+
+
+
