86241ac19bb390511915b0852c4e8c1f13c2b662
special4Party <spicial.party@gmail.com>
2022-05-27 21:27:22 +0300

    ass

---
+++ .history/include/php_page/index_20220527210747.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Home Page - Spical Party</title>
+    <link rel="stylesheet" href="../../layout/css/normalize.css">
+    <link rel="stylesheet" href="../../layout/css/cssstyleHome.css">
+</head>
+
+<body>
+    <!-- start header -->
+
+    <div class="header">
+        <div class="slider" style="height: 550px;
+    background-image: url(../../images/img/25.jpg);
+    background-size: cover;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    color: rgb(61, 22, 151);">
+            <div class="container">
+                <div class="intro">
+                    <h2>Welcome</h2>
+                    <button><a href="../../include/temp/choosing_type_signup.html"
+                            style="text-decoration-line:none;color: rgb(78, 11, 141);">
+                            Sign up</a></button>
+                    <button><a href="../../include/temp/login_all.html"
+                            style="text-decoration-line:none;color: rgb(78, 11, 141);">
+                            log in</a></button>
+                </div>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="container">
+
+                <h2> <span>Special</span> Party </h2>
+                <ul>
+                    <li class="active"><a href="../../include/temp/halls.html">Halls</a> </li>
+                    <li><a href="#Know-more">Know More</a></li>
+                    <li><a href="#about-us-link">About us</a></li>
+                    <li><a href="#contact">contact us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- End header -->
+
+    <!-- Start features -->
+    <div class="features">
+        <div class="container">
+            <div class="feat">
+                <h2> <a href="../../include/temp/some_information.html#wedding"
+                        style="text-decoration: none;">wedding</a></h2>
+                <p> From here, browse the most beautiful halls and exclusive offers for wedding halls and be
+                    distinguished with your choices.</p>
+            </div>
+            <div class="feat">
+                <h2> <a href="../../include/temp/some_information.html#Eng"
+                        style="text-decoration: none;">Engagement</a></h2>
+                <p> Do not search for the best wedding halls that we have collected for you here to announce your
+                    engagement in a very elegant way.</p>
+            </div>
+            <div class="feat">
+                <h2> <a href="../../include/temp/some_information.html#Party" style="text-decoration: none;">Party</a>
+                </h2>
+                <p> Whatever type of party you need to hold in a modern and different way, do not hesitate to browse
+                    this list.</p>
+            </div>
+        </div>
+    </div>
+    <!-- End features -->
+
+    <!-- Start About us -->
+    <div class="about-us">
+        <div class="container">
+            <div class="image">
+                <img src="../../images/img/35.jpg" alt="test">
+            </div>
+            <div class="info" id="about-us-link">
+                <h2> About us </h2>
+                <p>
+                    We are the place that every nervous person is looking for from organizing, arranging and booking an
+                    appointment or place for his wedding or engagement, or any kind of gatherings for weddings and
+                    beautiful occasions.
+                    Here we bring you between your hands wherever you are in this world calm and relaxation on the best
+                    day for you.</p>
+                <p>Here we will display your favorite wedding halls, and we will show you the services available for
+                    each hall, and all you have to do is select and choose your options. </p>
+                <div class="hobbies">
+                    <!-- start about box content -->
+                    <div class="content">
+                        <!-- start icon div-->
+                        <div class="icons">
+                            <img src="../../images/img/design4.png" alt="">
+                        </div>
+                        <!-- End icon div-->
+                        <!-- start text div-->
+                        <div class="text">
+                            <h3>Design</h3>
+                            <p>This section will take you to the halls that have the service of choosing a specific type
+                                of decoration.</p>
+                        </div>
+                        <!-- End text div-->
+                    </div>
+                    <!-- End about box content -->
+                    <!-- start about box content -->
+                    <div class="content">
+                        <!-- start icon div-->
+                        <div class="icons">
+                            <img src="../../images/img/food2.png" alt="">
+                        </div>
+                        <!-- End icon div-->
+                        <!-- start text div-->
+                        <div class="text">
+                            <h3>Food</h3>
+                            <p>This section will show you the halls in which the service of choosing the type of food
+                                served at the party is available.</p>
+                        </div>
+                        <!-- End text div-->
+                    </div>
+                    <!-- End about box content -->
+                    <!-- start about box content -->
+                    <div class="content">
+                        <!-- start icon div-->
+                        <div class="icons">
+                            <img src="../../images/img/c.png" alt="">
+                        </div>
+                        <!-- End icon div-->
+                        <!-- start text div-->
+                        <div class="text">
+                            <h3>Music</h3>
+                            <p>This section shows you the halls that offer a variety of song segments.</p>
+                        </div>
+                        <!-- End text div-->
+                    </div>
+                    <!-- End about box content -->
+                    <!-- start about box content -->
+                    <div class="content">
+                        <!-- start icon div-->
+                        <div class="icons">
+                            <img src="../../images/img/photo2.png" alt="">
+                        </div>
+                        <!-- End icon div-->
+                        <!-- start text div-->
+                        <div class="text">
+                            <h3>Photography</h3>
+                            <p>This section will show you the halls that provide photo sessions service during or before
+                                the party.</p>
+                        </div>
+                        <!-- End text div-->
+                    </div>
+                    <!-- End about box content -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End About us -->
+
+    <!-- Start skills -->
+    <div class="range">
+        <div class="container">
+            <div class="skills" id="contact">
+                <h2>what do you need?</h2>
+                <p>We can assist you at any time if you face any difficulty in using the site or in the booking process.
+                </p>
+                <p> We are here to make it easy for you. Be confident that you are in the best place.</p>
+                <button class="my-button"><a href="#contact-footer"
+                        style="text-decoration-line: none; color:blueviolet;">contact
+                        us</a></button>
+            </div>
+            <div class="progress">
+                <div class="tools">
+                    <h3>Jordanian weddings</h3>
+                    <div>
+                        <span style="width: 77%;">77% &nbsp</span>
+                    </div>
+                </div>
+
+                <div class="tools">
+                    <h3>Egyptian weddings</h3>
+                    <div>
+                        <span style="width: 70%;">70% &nbsp</span>
+                    </div>
+                </div>
+
+                <div class="tools">
+                    <h3>Syrian weddings</h3>
+                    <div>
+                        <span style="width: 85%;">85% &nbsp</span>
+                    </div>
+                </div>
+
+                <div class="tools">
+                    <h3>Palestinian weddings</h3>
+                    <div>
+                        <span style="width: 90%;">90% &nbsp</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Skils -->
+
+    <!-- Start resume -->
+    <div class="resume">
+        <div class="container">
+            <h2> But are you still thinking?! </h2>
+            <p>Start booking now and enjoy browsing the most powerful wedding halls, we promise that you will be amazed,
+                impressed and happy, let's go.</p>
+
+            <a href="../../include/temp/halls.html" class="Booking">Go to book now</a>
+        </div>
+    </div>
+    <!-- End resume -->
+
+    <!-- Start Education -->
+    <div class="my-education" id="Know-more">
+        <div class="container">
+            <div class="main">
+                <h2>Frequently asked questions :</h2>
+                <p> Frequently Asked Questions Inquiries are in your hands to shorten your time and effort and keep you
+                    comfortable. Enjoy your time.</p>
+                <img src="../../images/img/13.jpg">
+            </div>
+            <div class="expe">
+                <div class="section1">
+                    <!--   اس اس  سي تنسيقات اله ما 
+				 المحتوى لالم  بس الكلاس  هاد  -->
+                    <h3>What makes me choose a special party site to organize my party in it?</h3>
+                    <span><strong>Through a special party website, you can enjoy a reservation
+                            Your wedding hall in a measured time without fear of finding a wedding hall at certain
+                            times, such as the summer and spring, when occasions abound .
+                            in a simple way without going to the hall's geographical location and spending time and
+                            effort.</strong></span>
+                    <hr>
+                </div>
+                <div class="cestion1">
+                    <!--   اس اس  سي تنسيقات اله ما 
+				 المحتوى لالم  بس الكلاس  هاد  -->
+                    <h3>What are the site services?</h3>
+                    <span><strong>The site provides wedding and engagement party preparation services in the area chosen
+                            by the client and presents to him the services of each wedding hall that he reviews to
+                            choose in a flexible and easy way in terms of time and options available for each
+                            hall.</strong></span>
+                    <hr>
+                </div>
+                <!--   اس اس  سي تنسيقات اله ما 
+				 المحتوى لالم  بس الكلاس  هاد  -->
+                <div class="cestion2">
+                    <h3>What are the ways to join this site according to people?</h3>
+                    <span><strong>You can join the site as a client who wants to prepare his wedding or engagement party
+                            by placing some personal information during the login process, and you can join as a wedding
+                            hall owner by uploading official papers and documents during the login process as a wedding
+                            hall owner.</strong> </span>
+                    <hr>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- End Education -->
+
+    <!-- start test-imonias -->
+    <div class="test-imonias">
+        <div class="container">
+            <div class="scroll">
+                <div class="active-scroll">
+                    <q>The great marriages are partnerships. It can’t be a great marriage without being a
+                        partnership.</q>
+                    <h3>Helen Mirren</h3>
+                </div>
+                <div>
+                    <q>A successful marriage requires falling in love many times, always with the same person.</q>
+                    <h3>Mignon McLaughlin</h3>
+                </div>
+                <div>
+                    <q>A happy marriage is a long conversation which always seems too short.</q>
+                    <h3>Andre Maurois</h3>
+                </div>
+                <div>
+                    <q>Marriage is like watching the color of leaves in the fall, ever changing and more stunningly
+                        beautiful with each passing day.</q>
+                    <h3>Fawn Weaver</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End test-imonias -->
+
+    <!-- start portfulio -->
+    <div class="portfulio">
+        <div class="container">
+            <h2>Portfulio</h2>
+            <p>Here you can browse the most beautiful moments that were shared with our sweet customers in their
+                parties.</p>
+            <div class="our-work">
+                <div><img src="../../images/img/14.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/12.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/6.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/17.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/28.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/38.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/34.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/26.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/10.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/3.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/9.jpg" alt="test" width="200" height="200"></div>
+                <div><img src="../../images/img/a11.jpg" alt="test" width="200" height="200"></div>
+
+
+            </div>
+        </div>
+    </div>
+    <!-- End portfulio -->
+
+    <!-- start contact us-->
+    <div class="contact" id="contact-footer">
+        <div class="container">
+            <div class="info-contact">
+                <h2>Contact Us</h2>
+                <p>We are here a whole team to help you, feel free to ask any question here.
+                    We thank you for your time and effort and hope to achieve what you want.</p>
+                <p>
+                    - Online website -<br>
+                    Jordan - Amman
+                </p>
+                <p>
+                    <strong>Email-@ : </strong><a href="http://spicial.party@gmail.com">spicial.party@gmail.com</a><br>
+                    <strong>Phone : </strong>+9700473889
+                </p>
+            </div>
+            <?php
+			
+				require_once("../../conn.php");
+				if(isset($_POST["quistion_button_admin"])){
+					$email= $_POST["email_person"];
+                    $name= $_POST["name_person"];
+				}
+
+			?>
+            <form class="form" action="" method="post">
+                <label>Name : </label>
+                <input type="text" name="name_person" required>
+                <label>Email Address : </label>
+                <input type="email" name="email_person" required>
+                <label>Phone: </label>
+                <input type="tel" name="telphone" required>
+                <label>Note : </label>
+                <input type="text" name="note" style="width:100% ;" required>
+                <input type="submit" value=" Submit " name="quistion_button_admin">
+            </form>
+        </div>
+    </div>
+    <!-- End contact us -->
+
+    <!-- start footer -->
+    <div class="footer">
+        <div class="container">
+            <span>copyright &copy; 2022 wedding-hall</span>
+            <ul>
+                <li>
+                    <a href="http://facebook.com">
+                        <img src="../../images/img/face.png" alt="facebook">
+                    </a>
+                </li>
+                <li>
+                    <a href="http://web.whatsapp.com">
+                        <img src="../../images/img/what.png" alt="whatsapp">
+                    </a>
+                </li>
+                <li>
+                    <a href="http://accounts.snapchat.com">
+                        <img src="../../images/img/snap.png" alt="snapchat">
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.instagram.com">
+                        <img src="../../images/img/insta.png" alt="instagram">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- End footer-->
+    <script src="../../layout/js/jquery-3.6.0.min.js"></script>
+    <script src="../../layout/js/custome.js"></script>
+</body>
+
+</html>
